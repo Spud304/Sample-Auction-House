0a50c9df8b4fc55e99fc67f54c3435ce2222ace1
spud304 <hcprice115@gmail.com>
2022-07-30 21:13:41 -0400

    I am now reminded why I hate workflows

--- src/models.py
+++ src/models.py
@@ -28,38 +28,19 @@ class Listing(db.Model):
     def __repr__(self):
         return f'<Listing {self.item_id}>'
 
+class User(db.Model)
+    __tablename__ = 'users'
+    username = Column(String)
+    password = Column(String)
+    balance = Column(Integer)
+    user_id = Column(String, primary_key=True)
+
+    def __init__(self, username, password, balance, user_id):
+        self.username = username
+        self.password = password
+        self.balance = balance
+        self.user_id = user_id
 
 
-# mapper_registry = registry()
-
-# @mapper_registry.mapped
-# @dataclass
-# class Listing():
-#     def __init__(self, name, price, item_id, current_bid, highest_bidder, buy_now, seller):
-#         self.name = name
-#         self.price = price
-#         self.item_id = item_id
-#         self.current_bid = current_bid
-#         self.highest_bidder = highest_bidder
-#         self.buy_now = buy_now
-#         self.seller = seller
-
-
-#     __table__ = Table(
-#         "listings",
-#         mapper_registry.metadata,
-#         Column("name", String(100), nullable=False),
-#         Column("price", Integer, nullable=False),
-#         Column("item_id", String(36), primary_key=True, default=uuid4),
-#         Column("current_bid", Integer, nullable=False),
-#         Column("highest_bidder", String(36), nullable=True, default=uuid4),
-#         Column("buy_now", Integer, nullable=False),
-#         Column("seller", String(36), nullable=False, default=uuid4)
-#     )
-#     name: str = field(init=False)
-#     price: int = field(init=False)
-#     item_id: uuid4 = field(init=False)
-#     current_bid: int = field(init=False)
-#     highest_bidder: uuid4 = field(init=False)
-#     buy_now: int = field(init=False)
-#     seller: uuid4 = field(init=False)
+    def __repr__(self):
+        return f'<User {self.user_id}>'
